76629996d7c5be252d394f432423a2b9cf35f8b1
lokhandeamol43 <lokhandeamol43@gmail.com>
2023-05-06 13:05:43 +0530

    Create 01_variable_assign.js
    assignment 1

---
+++ 01_variable_assign.js
@@ -0,0 +1,34 @@
+var myFullName;
+console.log(myFullName);
+
+myFullName= "Amol Lokhande";
+console.log(myFullName);
+
+myFullName="Amol Pralhad Lokhande";
+console.log(myFullName);
+
+var myLove="I love only JS";
+console.log(myLove);
+
+var firstName="Amol";
+console.log(firstName);
+
+var lastName="Lokhande";
+console.log(lastName);
+
+var age="27";
+console.log(age);
+
+var address="Hadapsar";
+console.log(address);
+
+var pincode="411028";
+console.log(pincode);
+
+var gender="Male";
+console.log(gender);
+
+var isMarried="No";
+console.log(isMarried);
+
+// console.log(firstName, lastName, age, address, pincode, gender, isMarried )
