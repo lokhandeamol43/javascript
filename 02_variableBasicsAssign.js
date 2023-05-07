ed981f7ff10f3a759951bd61b8b3b6782e061eee
lokhandeamol43 <lokhandeamol43@gmail.com>
2023-05-07 23:22:43 +0530

    JavaScript assignment 2 and 3

---
+++ 02_variableBasicsAssign.js
@@ -0,0 +1,27 @@
+console.log("....#Number.......................................");
+var pin=411028;
+console.log("Pin:" +pin +"  Type:" +typeof Pin);
+var mob=996070;
+console.log("Mob:"+mob+"  Type:"+typeof mob);
+var cash=65000;
+console.log("Cash:"+cash+"  Type:"+typeof cash);
+console.log("");
+
+console.log("....#String.....................................");
+
+var name="Amol";
+console.log("Name:"+name+" Type:"+typeof name);
+var city="Pune";
+console.log("City:"+city+" Type:"+typeof city);
+var technology="Angular";
+console.log("Technology:"+technology+" Type:"+typeof technology);
+console.log("");
+
+console.log("....#Boolean...........................................");
+
+var isMarried=false;
+console.log("isMarried:"+isMarried+" Type:"+typeof isMarried);
+var isSelected=true;
+console.log("isSelected:"+isSelected+" Type:"+typeof isSelected);
+var isAppointed=true;
+console.log("isApppointed:"+isAppointed+" Type:"+typeof isAppointed);
