2a0c4f9d8c7e3da546ff02c6c4ef6259ed186977
lokhandeamol43 <lokhandeamol43@gmail.com>
2023-05-22 23:30:43 +0530

    Leap Year Assignment

--- leapYearAssignment.js
+++ leapYearAssignment.js
@@ -1,17 +1,17 @@
-var leapYear = function(year) {
-  if (typeof year === 'number' && !isNaN(year)) {
-    if (year % 4 === 0  && year % 100 !== 0 || year%  400 === 0) {
-      console.log(year + ' is a leap year.');
+var leapYear = function (year) {
+  if (typeof year === "number") {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      console.log(year + " is a leap year.");
     } else {
-      console.log(year + ' is not a leap year.');
+      console.log(year + " is not a leap year.");
     }
   } else {
-    console.log('Invalid input: ' + year + ' is not a valid year.');
+    console.log("Invalid input: " + year + " is not a valid year.");
   }
   console.log("---------------------------------------------------------------");
 }
 
-console.log("==================== Leap Year ==========================");
+console.log("======================== Leap Year =============================");
 console.log("");
 
 leapYear(2020);
@@ -26,6 +26,5 @@ leapYear(NaN);
 leapYear(1750);
 leapYear(1996);
 leapYear(1900);
-
 
 //(year % 4 === 0  && year % 100 !== 0 || year%  400 === 0)
