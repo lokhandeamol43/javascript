0a365a2391b56b8fd51e002e146a84fc958853b7
lokhandeamol43 <lokhandeamol43@gmail.com>
2023-06-03 17:12:22 +0530

    fibonacci

--- fibonacciSeries.js
+++ fibonacciSeries.js
@@ -1,28 +1,18 @@
-const a=10;
+const a=15;
 let num1=0;
 let num2=1;
 let num3;
-
+let num4="";
+let num5=" ";
 for(let i=1; i<=a; i++){
     
-    console.log(num1);
+    // console.log(num1);
+    num4=num4+num1+num5;
     num3=num1+num2;
     num1=num2;
     num2=num3;
-
+    
     //console.log(num1);
 }
-
-// function fibonacci(n) {
-//     let a = 0, b = 1, c;
-//     console.log(a);
-//     console.log(b);
-//     for (let i = 2; i < n; i++) {
-//       c = a + b;
-//       console.log(c);
-//       a = b;
-//       b = c;
-//     }
-//   }
-  
-// fibonacci(10);
+console.log("");
+console.log(`Fibonacci Series : ${num4}`);
